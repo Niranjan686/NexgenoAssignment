a923073d2bdb153aaa75af591217483b9da5e70b
Niranjan Shukla <niranjanshukla686@gmail.com>
2024-05-19 21:45:10 +0530

    dispaly image

--- taskify-frontend/src/components/Dashboard.jsx
+++ taskify-frontend/src/components/Dashboard.jsx
@@ -18,73 +18,75 @@ const Dashboard = () => {
         getUser();
     }, []);
 
-    return (
-        <section style={{ backgroundColor: '#eee' }}>
-            <div className="container py-5">
-                <div className="row">
-                    <div className="col-lg-4">
-                        <div className="card mb-4">
-                            <div className="card-body text-center">
-                                <img 
-                                    src={ 'https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp'} 
-                                    alt="avatar" 
-                                    className="rounded-circle img-fluid" 
-                                    style={{ width: '150px' }} 
-                                />
-                                <h5 className="my-3">{userdata.displayName || 'John Smith'}</h5>
-                                <p className="text-muted mb-1">Full Stack Developer</p>
-                                <p className="text-muted mb-4">Mumbai, Maharashtra</p>
-                                <div className="d-flex justify-content-center mb-2">
-                                    <button type="button" className="btn btn-primary">Edit</button>
+
+        return (
+            <section style={{ backgroundColor: '#eee' }}>
+                <div className="container py-5">
+                    <div className="row">
+                        <div className="col-lg-4">
+                            <div className="card mb-4">
+                                <div className="card-body text-center">
+                                    <img 
+                                        src={userdata?.image ?? 'https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp'} 
+                                        alt="avatar" 
+                                        className="rounded-circle img-fluid" 
+                                        style={{ width: '150px' }} 
+                                    />
+                                    <h5 className="my-3">{userdata?.displayName ?? 'John Smith'}</h5>
+                                    <p className="text-muted mb-1">Full Stack Developer</p>
+                                    <p className="text-muted mb-4">Mumbai, Maharashtra</p>
+                                    <div className="d-flex justify-content-center mb-2">
+                                        <button type="button" className="btn btn-primary">Edit</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="col-lg-8">
-                        <div className="card mb-4">
-                            <div className="card-body profile-info">
-                                <div className="row">
-                                    <div className="col-sm-3">
-                                        <p className="mb-0">Full Name</p>
+                        <div className="col-lg-8">
+                            <div className="card mb-4">
+                                <div className="card-body profile-info">
+                                    <div className="row">
+                                        <div className="col-sm-3">
+                                            <p className="mb-0">Full Name</p>
+                                        </div>
+                                        <div className="col-sm-9">
+                                            <p className="text-muted mb-0">{userdata?.displayName ?? 'Johnatan Smith'}</p>
+                                        </div>
                                     </div>
-                                    <div className="col-sm-9">
-                                        <p className="text-muted mb-0">{userdata.displayName || 'Johnatan Smith'}</p>
+                                    <hr />
+                                    <div className="row">
+                                        <div className="col-sm-3">
+                                            <p className="mb-0">Email</p>
+                                        </div>
+                                        <div className="col-sm-9">
+                                            <p className="text-muted mb-0">{userdata?.email ?? 'example@example.com'}</p>
+                                        </div>
                                     </div>
-                                </div>
-                                <hr />
-                                <div className="row">
-                                    <div className="col-sm-3">
-                                        <p className="mb-0">Email</p>
+                                    <hr />
+                                    <div className="row">
+                                        <div className="col-sm-3">
+                                            <p className="mb-0">Mobile</p>
+                                        </div>
+                                        <div className="col-sm-9">
+                                            <p className="text-muted mb-0">970--**884</p>
+                                        </div>
                                     </div>
-                                    <div className="col-sm-9">
-                                        <p className="text-muted mb-0">{userdata.email || 'example@example.com'}</p>
-                                    </div>
-                                </div>
-                                <hr />
-                                <div className="row">
-                                    <div className="col-sm-3">
-                                        <p className="mb-0">Mobile</p>
-                                    </div>
-                                    <div className="col-sm-9">
-                                        <p className="text-muted mb-0">970--**884</p>
-                                    </div>
-                                </div>
-                                <hr />
-                                <div className="row">
-                                    <div className="col-sm-3">
-                                        <p className="mb-0">Address</p>
-                                    </div>
-                                    <div className="col-sm-9">
-                                        <p className="text-muted mb-0">Mumbai, Maharashtra</p>
+                                    <hr />
+                                    <div className="row">
+                                        <div className="col-sm-3">
+                                            <p className="mb-0">Address</p>
+                                        </div>
+                                        <div className="col-sm-9">
+                                            <p className="text-muted mb-0">Mumbai, Maharashtra</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    );
+            </section>
+        );
+           
 };
 
 export default Dashboard;
