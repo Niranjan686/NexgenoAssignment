a7de2a2931b238effe886d6a11221e10de6f9210
Niranjan Shukla <niranjanshukla686@gmail.com>
2024-05-19 12:42:27 +0530

    Initial commit

---
+++ taskify-frontend/build/precache-manifest.2cdc238718b1612b6a765f4cfa5cb7c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a1f195b577b13cb5d4b0cb24d92570",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c669838af9a98ea43f5",
+    "url": "/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "89db63bd1351175913ff",
+    "url": "/static/css/main.ac63a8a1.chunk.css"
+  },
+  {
+    "revision": "0c669838af9a98ea43f5",
+    "url": "/static/js/2.40681dff.chunk.js"
+  },
+  {
+    "revision": "c22e7b4cb20d4556252c2aa78698d764",
+    "url": "/static/js/2.40681dff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89db63bd1351175913ff",
+    "url": "/static/js/main.afe598d9.chunk.js"
+  },
+  {
+    "revision": "519ed1765e221526865a",
+    "url": "/static/js/runtime-main.54609114.js"
+  }
+]);
---
+++ taskify-frontend/src/components/NavBar.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react'
+import "./NavBar.css"
+import { NavLink } from "react-router-dom"
+import axios from "axios"
+
+const Headers = () => {
+    const [userdata, setUserdata] = useState({});
+    console.log("response", userdata)
+
+
+    const getUser = async () => {
+        try {
+            const response = await axios.get("http://localhost:1000/login/sucess", { withCredentials: true });
+
+            setUserdata(response.data.user)
+        } catch (error) {
+            console.log("error", error)
+        }
+    }
+
+    // logoout
+    const logout = () => {
+        window.open("http://localhost:1000/logout", "_self")
+    }
+
+    useEffect(() => {
+        getUser()
+    }, [])
+    return (
+        <>
+            <header>
+                <nav>
+                    <div className="left">
+                        
+                            <img src='../Taskify.jpg' alt='Img not Found' style={{ height: "4em" }}  ></img>
+                        
+
+                    </div>
+                    <div className="right">
+                        <ul>
+                            
+                            {
+                                Object?.keys(userdata)?.length > 0 ? (
+                                    <>
+                                        <li>
+                                <NavLink to="/todo">
+                                    Home
+                                </NavLink>
+                            </li>
+                                        <li>
+                                            <NavLink to="/dashboard">
+                                                Profile
+                                            </NavLink>
+                                        </li>
+                                        
+                                        
+                                        <li onClick={logout}>Logout</li>
+                                    </>
+                                ) : <p></p>
+                            }
+
+
+
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Headers
