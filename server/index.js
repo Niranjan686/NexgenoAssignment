b8c758ef982ee448a251da0fa9bfe964c53b5e52
Niranjan Shukla <niranjanshukla686@gmail.com>
2024-05-19 13:16:26 +0530

    Deployed URL

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const clientsecret = process.env.CLIENTSECRET;
 
 
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:"https://taskify-frontend-rho.vercel.app",
     methods:"GET,POST,PUT,DELETE",
     credentials:true
 }));
@@ -73,8 +73,8 @@ passport.deserializeUser((user,done)=>{
 app.get("/auth/google",passport.authenticate("google",{scope:["profile","email"]}));
 
 app.get("/auth/google/callback",passport.authenticate("google",{
-    successRedirect:"http://localhost:3000/dashboard",
-    failureRedirect:"http://localhost:3000/login"
+    successRedirect:"https://taskify-frontend-rho.vercel.app/dashboard",
+    failureRedirect:"https://taskify-frontend-rho.vercel.app/login"
 }))
 
 app.get('/',(req,res)=>
@@ -93,7 +93,7 @@ app.get("/login/sucess",async(req,res)=>{
 app.get("/logout",(req,res,next)=>{
     req.logout(function(err){
         if(err){return next(err)}
-        res.redirect("http://localhost:3000");
+        res.redirect("https://taskify-frontend-rho.vercel.app");
     })
 })
 
